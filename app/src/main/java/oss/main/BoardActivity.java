dd79fe6735fb794503fd5e0490a5ca8d71053cfb
jeong-wonhyeong <sciencerhgkr@gmail.com>
2021-07-22 19:41:02 +0900

    wow

--- app/src/main/java/oss/main/BoardActivity.java
+++ app/src/main/java/oss/main/BoardActivity.java
@@ -4,18 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-/**
- *
- * @author PMS
- *
- * @modified Init -
- * @updated 21.07.22
- *
- *  보드 액티비티
- * 게시판 구현 화면
- *
- * @see oss.util.ActivityLauncher
- * */
 public class BoardActivity extends AppCompatActivity {
 
     @Override
