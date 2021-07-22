a6bbe4d922ff1db22f52abdcb1190b48ea276eb4
PushStone <76048647+pogihae@users.noreply.github.com>
2021-07-22 16:24:33 +0900

    Edit email_login

--- app/src/main/java/oss/main/BoardActivity.java
+++ app/src/main/java/oss/main/BoardActivity.java
@@ -4,6 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import oss.util.ActivityLauncher;
+
+/**
+ *
+ * @author PMS
+ *
+ * @modified Init -
+ * @updated 21.07.22
+ *
+ *  보드 액티비티
+ * 게시판 구현 화면
+ *
+ * @see ActivityLauncher
+ * */
 public class BoardActivity extends AppCompatActivity {
 
     @Override
