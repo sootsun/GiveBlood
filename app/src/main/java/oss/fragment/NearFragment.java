ecac330c211e793697e69f598bb3c35ef475a170
kyungcom <qrdlsgh1234@khu.ac.kr>
2021-08-04 17:01:22 +0900

    Update NearFragment.java

--- app/src/main/java/oss/fragment/NearFragment.java
+++ app/src/main/java/oss/fragment/NearFragment.java
@@ -1,8 +1,24 @@
 package oss.fragment;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-/*import com.google.android.gms.maps.CameraUpdateFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
@@ -11,24 +27,46 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;*/
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.pedro.library.AutoPermissionsListener;
+
+import oss.main.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link NearFragment#newInstance} factory method to
+ * Use the {@link NearFagment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NearFragment extends Fragment   {
-    /*private MapView mMapView;
+public class NearFagment extends Fragment {
+    private MapView mMapView;
     private GoogleMap googleMap;
+
+    LocationManager manager;
+    Double latitude = 0.0;
+    Double longitude = 0.0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_near, null, false);
+        int permissionCheck = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION);
+        int permissionCheck2 = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION);
+        if(permissionCheck == PackageManager.PERMISSION_DENIED){ //위치 권한 확인
+
+            //위치 권한 요청
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        }
+        if(permissionCheck2 == PackageManager.PERMISSION_DENIED){ //위치 권한 확인
+
+            //위치 권한 요청
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+        }
+        startLocation();
         mMapView = (MapView) view.findViewById(R.id.map);
         mMapView.onCreate(savedInstanceState);
         mMapView.onResume();
+
+
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
         } catch (Exception e) {
@@ -39,7 +77,7 @@ public class NearFragment extends Fragment   {
             public void onMapReady(GoogleMap mMap) {
                 googleMap = mMap;
                 //To add marker
-                LatLng sydney = new LatLng(37.52487, 126.92723);
+                LatLng sydney = new LatLng(latitude, longitude);
                 googleMap.addMarker(new MarkerOptions().position(sydney).title("Title").snippet("Marker Description"));
                 // For zooming functionality
                 CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
@@ -48,6 +86,7 @@ public class NearFragment extends Fragment   {
         });
         return view;
     }
+
 
     @Override
     public void onResume() {
@@ -70,5 +109,48 @@ public class NearFragment extends Fragment   {
         mMapView.onLowMemory();
     }
 
-*/
+    private void startLocation(){
+        manager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        long minTime = 1000;
+        float minDistance = 1;
+
+        if(ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED &&
+        ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ){
+            Toast.makeText(getActivity(),"DONT HAVE PERMISSIONS",Toast.LENGTH_LONG).show();
+            return;
+        }
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, mLocationListener);
+        manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,minTime, minDistance, mLocationListener);
+    }
+
+    private void stopLocationService(){
+        if(ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ){
+            Toast.makeText(getActivity(),"DONT HAVE PERMISSIONS", Toast.LENGTH_LONG).show();
+            return;
+        }
+        manager.removeUpdates(mLocationListener);
+    }
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+
+            stopLocationService();
+        }
+
+        public void onProviderDisabled(String provider){}
+
+        public void onProviderEnabled(String provider){}
+
+        public void onStatusChanged(String provider, int status, Bundle extras){}
+    };
+
+
 }
+
